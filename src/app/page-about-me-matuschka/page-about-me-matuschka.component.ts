1843d3c4634f5807c2b733400dc84958d7250c4f
Simon Matuschka <210062@studierende.htl-donaustadt.at>
2024-11-26 11:20:48 +0100

    Matuschka commit test

---
+++ src/app/page-about-me-matuschka/page-about-me-matuschka.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-page-about-me-matuschka',
+  imports: [],
+  templateUrl: './page-about-me-matuschka.component.html',
+  styleUrl: './page-about-me-matuschka.component.scss'
+})
+export class PageAboutMeMATUSCHKAComponent {
+
+}
