c26fd7c8bb6a122dd0e4ff8e167d23de902fd03e
Askaloor <alexander.millich1@gmail.com>
2024-11-26 10:50:49 +0100

    MILLICH erster Commit-Test

---
+++ src/app/page-about-me-millich/page-about-me-millich.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-page-about-me-millich',
+  imports: [],
+  templateUrl: './page-about-me-millich.component.html',
+  styleUrl: './page-about-me-millich.component.scss'
+})
+export class PageAboutMeMILLICHComponent {
+
+}
