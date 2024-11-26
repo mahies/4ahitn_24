ecc704b3e5309859c1f67530a19b8d93400df92c
Askaloor <alexander.millich1@gmail.com>
2024-11-26 11:09:30 +0100

    MILLICH Link zu about me

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import {PageDemoComponent} from './page-demo/page-demo.component';
 import {PageHomeComponent} from './page-home/page-home.component';
 import {PageAboutMeKRAPFComponent} from './page-about-me-krapf/page-about-me-krapf.component';
+import {PageAboutMeMILLICHComponent} from './page-about-me-millich/page-about-me-millich.component';
 
 export const routes: Routes = [
   { path: '', // default route
@@ -12,5 +13,7 @@ export const routes: Routes = [
     component: PageDemoComponent },
   {path: 'about_me_krapf',
     component: PageAboutMeKRAPFComponent },
+  { path: 'about_meMILLICH',
+    component: PageAboutMeMILLICHComponent}
 ];
 
