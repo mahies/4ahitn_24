07e6e5e5db3eaf8fc0b3c0a87c235607050e73fb
Adryano <adryanobutrus@gmail.com>
2024-11-27 09:36:04 +0100

    YOHANNA-Second Push

--- src/app/page-about-me-yohanna/page-about-me-yohanna.component.ts
+++ src/app/page-about-me-yohanna/page-about-me-yohanna.component.ts
@@ -4,8 +4,15 @@ import { Component } from '@angular/core';
   selector: 'app-page-about-me-yohanna',
   imports: [],
   templateUrl: './page-about-me-yohanna.component.html',
+  standalone: true,
   styleUrl: './page-about-me-yohanna.component.scss'
 })
 export class PageAboutMeYOHANNAComponent {
-
+  vorname: string = "Adryano"
+  nachname: string = "Yohanna"
+  geburtsdatum: string = "25.10.2006"
+  abteilung: string = "Abteoilung"
+  github_username: string = "qaxry"
+  wohnort: string = "1210 Wien"
+  uebermich: string = "Text über mich"
 }
