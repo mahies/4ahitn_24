7cf93ce3615edbf7003e1437c1d838fe073ef156
MatteoHajduk <210273@studierende.htl-donaustadt.at>
2024-12-03 10:23:25 +0100

    Code von Hies eingefügt und geändert (hatte am anfang fehler --> innerhalb der klasse eingefügt)

--- src/app/page-about-me-klaric/page-about-me-klaric.component.ts
+++ src/app/page-about-me-klaric/page-about-me-klaric.component.ts
@@ -1,5 +1,29 @@
 import { Component } from '@angular/core';
+export interface Person {
+  surname: string;
+  firstname: string;
+  urlImage: string;
+  birthday: Date;
+  department: string;
+  email: string;
+  github_nickname: string;
+  zip_code: number; // äquivalent zu Integer in Java
+  city: string;
+  message: string;
+}
 
+export const MINNIE: Person = {
+  surname: "KLARIC",
+  firstname: "Matteo",
+  urlImage: "assets/demo/Minnie_Mouse.webp",
+  birthday: new Date("2005-07-31"),
+  department: "ITEL Netzwerktechnik",
+  email: "210273@studierende.htl-donaustadt.at",
+  github_nickname: "MatteoHajduk",
+  zip_code: 1010,
+  city: "Wien",
+  message: "\"Reden ist silber Schweigen ist Gold\" \n(KLARIC)"
+};
 @Component({
   selector: 'app-page-about-me-klaric',
   imports: [],
@@ -7,5 +31,8 @@ import { Component } from '@angular/core';
   styleUrl: './page-about-me-klaric.component.scss'
 })
 export class PageAboutMeKLARICComponent {
-
 }
+
+
+
+
