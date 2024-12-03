b8c5953d95b3aab23a641628cf728240662e50f8
mahi <hies@htl-donaustadt.at>
2024-12-03 08:50:49 +0100

    reformat code

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,4 @@
-import { Routes } from '@angular/router';
+import {Routes} from '@angular/router';
 import {PageDemoComponent} from './page-demo/page-demo.component';
 import {PageHomeComponent} from './page-home/page-home.component';
 import {PageAboutMeKRAPFComponent} from './page-about-me-krapf/page-about-me-krapf.component';
@@ -6,29 +6,49 @@ import {PageAboutMeMILLICHComponent} from './page-about-me-millich/page-about-me
 import {PageAboutMeKLARICComponent} from './page-about-me-klaric/page-about-me-klaric.component';
 import {PageAboutMeAljarrahComponent} from './page-about-me-aljarrah/page-about-me-aljarrah.component';
 import {PageAboutMeGEIHSEDERComponent} from './page-about-me-geihseder/page-about-me-geihseder.component';
-import {PageAboutMeYOHANNAComponent}  from './page-about-me-yohanna/page-about-me-yohanna.component';
+import {PageAboutMeYOHANNAComponent} from './page-about-me-yohanna/page-about-me-yohanna.component';
 import {PageAboutMeMATUSCHKAComponent} from './page-about-me-matuschka/page-about-me-matuschka.component';
 
 export const routes: Routes = [
-  { path: '', // default route
-    component: PageHomeComponent },
-  { path: 'home',
-    component: PageHomeComponent },
-  { path: 'page_demo',
-    component: PageDemoComponent },
-  {path: 'about_me_krapf',
-    component: PageAboutMeKRAPFComponent },
-  { path: 'about_meMILLICH',
-    component: PageAboutMeMILLICHComponent},
-  {path: 'aboutMeKLARIC',
-  component: PageAboutMeKLARICComponent},
-  {path: 'about_me_aljarrah',
-    component: PageAboutMeAljarrahComponent },
-  {path: 'aboutMeGEIHSEDER',
-    component: PageAboutMeGEIHSEDERComponent},
-  {path: 'aboutMeYOHANNA',
-    component: PageAboutMeYOHANNAComponent},
-  {path: 'aboutMeMATUSCHKA',
-    component: PageAboutMeMATUSCHKAComponent}
+  {
+    path: '', // default route
+    component: PageHomeComponent
+  },
+  {
+    path: 'home',
+    component: PageHomeComponent
+  },
+  {
+    path: 'page_demo',
+    component: PageDemoComponent
+  },
+  {
+    path: 'about_me_krapf',
+    component: PageAboutMeKRAPFComponent
+  },
+  {
+    path: 'about_meMILLICH',
+    component: PageAboutMeMILLICHComponent
+  },
+  {
+    path: 'aboutMeKLARIC',
+    component: PageAboutMeKLARICComponent
+  },
+  {
+    path: 'about_me_aljarrah',
+    component: PageAboutMeAljarrahComponent
+  },
+  {
+    path: 'aboutMeGEIHSEDER',
+    component: PageAboutMeGEIHSEDERComponent
+  },
+  {
+    path: 'aboutMeYOHANNA',
+    component: PageAboutMeYOHANNAComponent
+  },
+  {
+    path: 'aboutMeMATUSCHKA',
+    component: PageAboutMeMATUSCHKAComponent
+  }
 ];
 
