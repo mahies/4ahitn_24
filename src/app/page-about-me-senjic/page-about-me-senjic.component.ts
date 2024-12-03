d7233222332f7b60444d6b1f96aa3787dfcc91ec
olive <210278@studierende.htl-donaustadt.at>
2024-12-03 10:26:33 +0100

    Portrait oben rechts eingefügt

--- src/app/page-about-me-senjic/page-about-me-senjic.component.ts
+++ src/app/page-about-me-senjic/page-about-me-senjic.component.ts
@@ -8,4 +8,9 @@ import { Component } from '@angular/core';
 })
 export class PageAboutMeSenjicComponent {
   name:String = "SENJIC";
+  imagePath: string = 'assets-SENJIC/fghjkdibfeb.png';
+
+
 }
+
+
