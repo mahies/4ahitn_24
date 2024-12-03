647419377ab427807d6eb80000479b9596808eea
mahi <hies@htl-donaustadt.at>
2024-12-03 08:38:27 +0100

    add model, mock data, use mocked data

--- src/app/page-demo/page-demo.component.ts
+++ src/app/page-demo/page-demo.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import {Person, MINNIE} from '../model/person';
+import {RouterLink, RouterLinkActive} from '@angular/router';
 
 @Component({
   selector: 'app-page-demo',
-  imports: [],
+  imports: [RouterLink,RouterLinkActive],
   templateUrl: './page-demo.component.html',
   styleUrl: './page-demo.component.scss',
   standalone: true
