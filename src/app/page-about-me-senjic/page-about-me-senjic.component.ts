0cc3c7457eeeeedc85b3464cb51421455c12d903
olive <210278@studierende.htl-donaustadt.at>
2024-12-03 08:50:59 +0100

    Test webseite

---
+++ src/app/page-about-me-senjic/page-about-me-senjic.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-page-about-me-senjic',
+  imports: [],
+  templateUrl: './page-about-me-senjic.component.html',
+  styleUrl: './page-about-me-senjic.component.scss'
+})
+export class PageAboutMeSenjicComponent {
+  name:String = "SENJIC";
+}
