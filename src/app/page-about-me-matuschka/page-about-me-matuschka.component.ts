f5a54a80bab43acebfaf5ba48f19edc1f40725ab
Simon Matuschka <210062@studierende.htl-donaustadt.at>
2024-12-18 09:12:28 +0100

    UEB-28

--- src/app/page-about-me-matuschka/page-about-me-matuschka.component.ts
+++ src/app/page-about-me-matuschka/page-about-me-matuschka.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import {RouterLink, RouterLinkActive} from '@angular/router';
 
 @Component({
   selector: 'app-page-about-me-matuschka',
-  imports: [],
+  imports: [RouterLink,RouterLinkActive],
   templateUrl: './page-about-me-matuschka.component.html',
   styleUrl: './page-about-me-matuschka.component.scss'
 })
