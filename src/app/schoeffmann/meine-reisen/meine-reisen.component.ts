8fc749b6e9d7a4a747172cae18c82d129d3dd128
theBroGrammer2624 <anonymistdumm@gmail.com>
2025-01-07 13:51:20 +0100

    AP-12 AP-13

--- src/app/schoeffmann/meine-reisen/meine-reisen.component.ts
+++ src/app/schoeffmann/meine-reisen/meine-reisen.component.ts
@@ -10,8 +10,12 @@ import {RouterLink, RouterLinkActive} from '@angular/router';
 export class MeineReisenComponent {
 uptipter:String="Uptipter ist ein Paradies mit kristallklarem Wasser und beeindruckender Küstenlandschaft";
 oesterreich:String="Österreich ist ein schnönes Land";
+norwegen:String="Norwegen ist ein winterliches Land";
+usa:String="Die USA hat viele coole Brücken";
 
 uptipterimg: string = "assets-SCHOEFFMANN/uptipter.webp";
 oesterreichimg: string = "assets-SCHOEFFMANN/oesterreich.png";
+norwegenimg: string = "assets-SCHOEFFMANN/norwegen-winter.jpg";
+usaimg: string = "assets-SCHOEFFMANN/usa-bruecke.jpg";
 
 }
