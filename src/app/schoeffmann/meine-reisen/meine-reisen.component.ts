238db3ee91610abe2e026ab8179d01d76e905058
theBroGrammer2624 <anonymistdumm@gmail.com>
2025-01-07 11:31:00 +0100

    AP-8

---
+++ src/app/schoeffmann/meine-reisen/meine-reisen.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-meine-reisen',
+  imports: [],
+  templateUrl: './meine-reisen.component.html',
+  styleUrl: './meine-reisen.component.scss'
+})
+export class MeineReisenComponent {
+
+}
