b18d03d50e7035d84f5655a2121a84a1e4070bca
Adryano <adryanobutrus@gmail.com>
2025-01-08 09:07:10 +0100

    UB-37

--- src/app/model/person.ts
+++ src/app/model/person.ts
@@ -68,12 +68,12 @@ export const yohanna : Person = {
   surname: "Yohanna",
   firstname: "Adryano",
   urlImage: "assets-YOHANNA/Bild_YOHANNA.jpg",
-  birthday: new Date("2006-11-25"),
+  birthday: new Date("2006-10-25"),
   department: "ITEL Netzwerktechnik",
   email: "210424@studierende.htl-donaustadt.at",
   github_nickname: "qaxry",
   zip_code: 1210,
   city: "Wien",
-  message: "\„One day or day one. You decide.\" \n(YOHANNA Adryano)"
+  message: "\„One day or day one.\" \n(YOHANNA Adryano)"
 }
 
--- src/app/page-about-me-yohanna/page-about-me-yohanna.component.ts
+++ src/app/page-about-me-yohanna/page-about-me-yohanna.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import {Person, yohanna} from '../model/person';
 import {RouterLink, RouterLinkActive} from '@angular/router';
+import {DatePipe} from '@angular/common';
 @Component({
   selector: 'app-page-about-me-yohanna',
   imports: [
     RouterLink,
-    RouterLinkActive
+    RouterLinkActive,
+    DatePipe
   ],
   templateUrl: './page-about-me-yohanna.component.html',
   standalone: true,
