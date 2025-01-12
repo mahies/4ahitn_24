242dc0515917a17f4447297dd39ded70d7a5256e
Simon Matuschka <210062@studierende.htl-donaustadt.at>
2025-01-12 22:57:38 +0100

    UEB-34

--- src/app/page-MATUSCHKA/page-stadienbesuche-matuschka/page-stadienbesuche-matuschka.component.ts
+++ src/app/page-MATUSCHKA/page-stadienbesuche-matuschka/page-stadienbesuche-matuschka.component.ts
@@ -1,8 +1,12 @@
 import { Component } from '@angular/core';
+import {RouterLink, RouterLinkActive} from "@angular/router";
 
 @Component({
   selector: 'app-page-stadienbesuche-matuschka',
-  imports: [],
+    imports: [
+        RouterLink,
+        RouterLinkActive
+    ],
   templateUrl: './page-stadienbesuche-matuschka.component.html',
   styleUrl: './page-stadienbesuche-matuschka.component.scss'
 })
