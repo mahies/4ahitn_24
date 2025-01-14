8c93285dd2a5819b51545d74a0c855005412d27f
Simon Matuschka <210062@studierende.htl-donaustadt.at>
2025-01-14 10:51:29 +0100

    Model hinzugefügt

--- src/app/page-MATUSCHKA/page-about-me-matuschka/page-about-me-matuschka.component.ts
+++ src/app/page-MATUSCHKA/page-about-me-matuschka/page-about-me-matuschka.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import {RouterLink, RouterLinkActive} from '@angular/router';
+import {Person, matuschka} from '../../model/person';
+
 
 @Component({
   selector: 'app-page-about-me-matuschka',
@@ -8,6 +10,7 @@ import {RouterLink, RouterLinkActive} from '@angular/router';
   styleUrl: './page-about-me-matuschka.component.scss'
 })
 export class PageAboutMeMATUSCHKAComponent {
+  me: Person = matuschka;
   name : string = "Matuschka";
   Mail: string = "210062@studierende.htl-donaustadt.at";
   Github: string = "Simon3294";
