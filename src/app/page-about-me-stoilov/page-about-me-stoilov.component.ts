8401a8adabd0897e31d78fb05c53e3f4b097d6ba
Daniel <210417@studierende.htl-donaustadt.at>
2025-01-14 11:45:22 +0100

    Über mich weitergearbeitet

--- src/app/page-about-me-stoilov/page-about-me-stoilov.component.ts
+++ src/app/page-about-me-stoilov/page-about-me-stoilov.component.ts
@@ -7,5 +7,10 @@ import { Component } from '@angular/core';
   styleUrl: './page-about-me-stoilov.component.scss'
 })
 export class PageAboutMeStoilovComponent {
-
+  firstName : string = "Daniel";
+  lastName : string = "STOILOV"
+  email: string = "210417@studierende.htl-donaustadt.at";
+  github: string = "@Danielzis";
+  birthDate : string = "05.12.2005";
+  profileImage : string = "assets-STOILOV/Ich.jpg";
 }
