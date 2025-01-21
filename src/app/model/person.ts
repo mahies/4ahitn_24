4caf96b1a05aa982816e05b8d4ca9d820bec652b
Askaloor <alexander.millich1@gmail.com>
2025-01-21 10:24:35 +0100

    MILLICH: UB-56

--- src/app/model/person.ts
+++ src/app/model/person.ts
@@ -53,7 +53,7 @@ export  const klaric : Person = {
   message: "\„Work so hard that your idols become your rivals\" \n(KLARIC Matteo)"
 };
 
-export const millich : Person = {
+export const MILLICH : Person = {
   surname: "Millich",
   firstname: "Alexander",
   urlImage: "assets-MILLICH/Bild_MILLICH.jpg",
