f3650362ceddd9b18c16a12bcbb03a0acbf7b14d
olive <210278@studierende.htl-donaustadt.at>
2025-01-21 17:36:44 +0100

    UEB-10: Werdegang

--- src/app/pages/pages-students/page-SENJIC/page-my-future-senjic/page-my-future-senjic.component.ts
+++ src/app/pages/pages-students/page-SENJIC/page-my-future-senjic/page-my-future-senjic.component.ts
@@ -1,11 +1,19 @@
 import { Component } from '@angular/core';
+import {RouterLink, RouterLinkActive} from "@angular/router";
 
 @Component({
   selector: 'app-page-my-future-senjic',
-  imports: [],
+    imports: [
+        RouterLink,
+        RouterLinkActive
+    ],
   templateUrl: './page-my-future-senjic.component.html',
   styleUrl: './page-my-future-senjic.component.scss'
 })
 export class PageMyFutureSenjicComponent {
+
+  Mengergasse: string = "2013 – 2017 ";
+  F26: string = "2017 – 2021";
+  HTL: string = "2021 - laufend";
 
 }
