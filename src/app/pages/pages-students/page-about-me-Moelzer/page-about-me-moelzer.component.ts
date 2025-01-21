ad2098ebf88a3879d55b92bffbfe650ddbe0ba5f
onesowngoal <Patrick.moelzer@gmail.com>
2025-01-21 08:52:57 +0100

    updatemv2

--- src/app/pages/pages-students/page-about-me-Moelzer/page-about-me-moelzer.component.ts
+++ src/app/pages/pages-students/page-about-me-Moelzer/page-about-me-moelzer.component.ts
@@ -1,14 +1,11 @@
 import { Component } from '@angular/core';
 import {Person, moelzer} from '../../../model/person';
 import {RouterLink, RouterLinkActive} from '@angular/router';
-import {DatePipe} from '@angular/common';
 @Component({
   selector: 'app-page-about-me-moelzer',
   imports: [
     RouterLink,
     RouterLinkActive,
-    DatePipe,
-
   ],
   templateUrl: './page-about-me-moelzer.component.html',
   styleUrl: './page-about-me-moelzer.component.scss'
