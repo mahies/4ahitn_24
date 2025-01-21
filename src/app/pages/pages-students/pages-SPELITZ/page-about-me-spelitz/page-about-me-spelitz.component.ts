eab5ddec1c7cc007523ba91473688d888ddc1bfd
BuildTools <unconfigured@null.spigotmc.org>
2025-01-21 21:29:07 +0100

    SPELITZ FS-11

--- src/app/pages/pages-students/pages-SPELITZ/page-about-me-spelitz/page-about-me-spelitz.component.ts
+++ src/app/pages/pages-students/pages-SPELITZ/page-about-me-spelitz/page-about-me-spelitz.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
 import {MILLICH, Person, SPELITZ} from '../../../../model/person';
 import {DatePipe} from '@angular/common';
+import {RouterLink, RouterLinkActive} from '@angular/router';
 
 @Component({
   selector: 'app-page-about-me-spelitz',
   imports: [
-    DatePipe
+    DatePipe,
+    RouterLink,
+    RouterLinkActive
   ],
   templateUrl: './page-about-me-spelitz.component.html',
   styleUrl: './page-about-me-spelitz.component.scss'
