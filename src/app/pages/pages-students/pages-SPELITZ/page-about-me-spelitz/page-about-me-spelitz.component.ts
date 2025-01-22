4c98d5e9971cceae5ba2701b0a5c2ca651fe49f9
mahi <hies@htl-donaustadt.at>
2025-01-22 08:56:40 +0100

    Revert "SPELITZ FS-11"
    This reverts commit eab5ddec1c7cc007523ba91473688d888ddc1bfd.

--- src/app/pages/pages-students/pages-SPELITZ/page-about-me-spelitz/page-about-me-spelitz.component.ts
+++ src/app/pages/pages-students/pages-SPELITZ/page-about-me-spelitz/page-about-me-spelitz.component.ts
@@ -1,14 +1,11 @@
 import { Component } from '@angular/core';
 import {MILLICH, Person, SPELITZ} from '../../../../model/person';
 import {DatePipe} from '@angular/common';
-import {RouterLink, RouterLinkActive} from '@angular/router';
 
 @Component({
   selector: 'app-page-about-me-spelitz',
   imports: [
-    DatePipe,
-    RouterLink,
-    RouterLinkActive
+    DatePipe
   ],
   templateUrl: './page-about-me-spelitz.component.html',
   styleUrl: './page-about-me-spelitz.component.scss'
