97d431b4b845784ac93d96200bdd65ae3574ea35
Simon Matuschka <210062@studierende.htl-donaustadt.at>
2025-02-11 09:00:48 +0100

    UEB-40

--- src/app/pages/pages-students/page-MATUSCHKA/page-meine-reisen-matuschka/page-meine-reisen-matuschka.component.ts
+++ src/app/pages/pages-students/page-MATUSCHKA/page-meine-reisen-matuschka/page-meine-reisen-matuschka.component.ts
@@ -1,8 +1,12 @@
 import { Component } from '@angular/core';
+import {RouterLink, RouterLinkActive} from '@angular/router';
 
 @Component({
   selector: 'app-page-meine-reisen-matuschka',
-  imports: [],
+  imports: [
+    RouterLink,
+    RouterLinkActive
+  ],
   templateUrl: './page-meine-reisen-matuschka.component.html',
   styleUrl: './page-meine-reisen-matuschka.component.scss'
 })
