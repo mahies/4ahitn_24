e3f4ba35bf43bc6367783a3f8af1a64da48b60c2
sebastian447 <210031@studierende.htl-donaustadt.at>
2025-02-12 08:43:49 +0100

    subpages hinzugefügt

---
+++ src/app/pages/pages-students/pages-GEIHSEDER/meine-reisen-geihseder/meine-reisen-geihseder.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-meine-reisen-geihseder',
+  templateUrl: './meine-reisen-geihseder.component.html',
+  styleUrl: './meine-reisen-geihseder.component.scss'
+})
+export class MeineReisenGeihsederComponent {
+
+}
