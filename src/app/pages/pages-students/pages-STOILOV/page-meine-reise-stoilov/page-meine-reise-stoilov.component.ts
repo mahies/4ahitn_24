8e6ec40a6fe9cdb5e150f634067222ee2fdf7a7c
Daniel <210417@studierende.htl-donaustadt.at>
2025-02-14 17:38:44 +0100

    UEB-23 bis UEB-35

--- src/app/pages/pages-students/pages-STOILOV/page-meine-reise-stoilov/page-meine-reise-stoilov.component.ts
+++ src/app/pages/pages-students/pages-STOILOV/page-meine-reise-stoilov/page-meine-reise-stoilov.component.ts
@@ -1,8 +1,12 @@
 import { Component } from '@angular/core';
+import {RouterLink, RouterLinkActive} from "@angular/router";
 
 @Component({
   selector: 'app-page-meine-reise-stoilov',
-  imports: [],
+    imports: [
+        RouterLink,
+        RouterLinkActive
+    ],
   templateUrl: './page-meine-reise-stoilov.component.html',
   styleUrl: './page-meine-reise-stoilov.component.scss'
 })
