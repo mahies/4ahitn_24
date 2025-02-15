2b65f23f2c78375efda362eb1bb0ae25b6e6c8b5
spelitz <fabianspelitz06@gmail.com>
2025-02-15 22:11:45 +0100

    routes

--- src/app/pages/pages-students/pages-SPELITZ/meine-reisen-spelitz/meine-reisen-spelitz.component.ts
+++ src/app/pages/pages-students/pages-SPELITZ/meine-reisen-spelitz/meine-reisen-spelitz.component.ts
@@ -1,8 +1,12 @@
 import { Component } from '@angular/core';
+import {RouterLink, RouterLinkActive} from '@angular/router';
 
 @Component({
   selector: 'app-meine-reisen-spelitz',
-  imports: [],
+  imports: [
+    RouterLink,
+    RouterLinkActive
+  ],
   templateUrl: './meine-reisen-spelitz.component.html',
   styleUrl: './meine-reisen-spelitz.component.scss'
 })
