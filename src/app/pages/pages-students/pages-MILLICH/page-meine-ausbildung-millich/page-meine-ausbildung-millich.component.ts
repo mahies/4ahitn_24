8813009ba634841a1cd901f6cccc872c65b5078d
Askaloor <alexander.millich1@gmail.com>
2025-02-16 16:44:04 +0100

    MILLICH: UB-46

--- src/app/pages/pages-students/pages-MILLICH/page-meine-ausbildung-millich/page-meine-ausbildung-millich.component.ts
+++ src/app/pages/pages-students/pages-MILLICH/page-meine-ausbildung-millich/page-meine-ausbildung-millich.component.ts
@@ -1,8 +1,12 @@
 import { Component } from '@angular/core';
+import {RouterLink, RouterLinkActive} from '@angular/router';
 
 @Component({
   selector: 'app-page-meine-ausbildung-millich',
-  imports: [],
+  imports: [
+    RouterLink,
+    RouterLinkActive
+  ],
   templateUrl: './page-meine-ausbildung-millich.component.html',
   styleUrl: './page-meine-ausbildung-millich.component.scss'
 })
