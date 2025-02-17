ffe7d9c8dfe54eed4914da48657410f4e67ed871
karinaB777 <becker.karina@outlook.com>
2025-02-17 23:46:25 +0100

    userstorie 28

--- src/app/pages/pages-students/page-BECKER/page-about-meine-reisen-becker/page-about-meine-reisen-becker.component.ts
+++ src/app/pages/pages-students/page-BECKER/page-about-meine-reisen-becker/page-about-meine-reisen-becker.component.ts
@@ -11,17 +11,28 @@ import { CommonModule } from '@angular/common';
 export class PageAboutMeineReisenBeckerComponent {
   visitedDestinations = [
     {
-      name: 'St. Johann, Salzburg',
-      description: ''
+      name: 'Nassfeld, Kärnten',
+      images: ['assets-BECKER/skipiste.jpeg', 'assets-BECKER/ski.jpeg', 'assets-BECKER/aussichtbalkonskiwoche.jpeg'],
+      description: 'Skikurs Dezember 2022: Mein erstes Skifahren war wirklich etwas Besonderes. Wir hatten super schöne Tage und unglaublich viel Spaß. '
     },
     {
       name: 'Polen',
-      description: ''
+      images: ['assets-BECKER/ciesznstraße.jpeg', 'assets-BECKER/gdansk.jpeg', 'assets-BECKER/meergdansk.jpeg',
+        'assets-BECKER/panzer.jpeg', 'assets-BECKER/strandleba.jpeg', 'assets-BECKER/westerplatte.jpeg'],
+      description: 'Jeden Sommer in Polen: Wir erkunden verschiedene Städte, oft an der schönen Ostsee. Egal ob in der lebendigen Großstadt Gdansk (Danzig) oder in den kleineren Städten wie Lebork und Leba, es ist immer eine Freude, all die Sehenswürdigkeiten zu entdecken und die Vielfalt der Orte zu erleben.'
     },
     {
-      name: 'Nassfeld, Kärnten',
-      description: ''
-    }
-    // Füge hier weitere Reiseziele hinzu
+      name: 'Burgenland',
+      images: ['assets-BECKER/hochseilgarten.jpeg', 'assets-BECKER/dirtbike.jpeg',],
+      description: 'Burgenland, Juli 2024: entspanntes Wochenende im Burgenland. Der Hochseilklettergarten war ein spannendes Highlight, bei dem wir hoch hinausgingen und viel Spaß hatten. Ein perfekter Mix aus Entspannung und Abenteuer!'
+    },
+    {
+      name: 'Salzburg',
+      images: [
+        'assets-BECKER/flying fox.jpeg', 'assets-BECKER/klamm.jpeg', 'assets-BECKER/klammschlucht.jpeg',
+        'assets-BECKER/klammtreppe.jpeg', 'assets-BECKER/regenbogen.jpeg', 'assets-BECKER/wasserfall.jpeg'],
+      description: 'Salzburg, Juli 2024: Dieser Urlaub war ein unglaubliches Abenteuer! Besonders der Flying Fox war ein absolutes Highlight. Ein toller Urlaub mit unvergesslichen Erinnerungen und vielen tollen Aktivitäten, wie der wunderschöne Liechtensteinklamm und die Krimmler Wasserfälle.'
+    },
+
   ];
 }
