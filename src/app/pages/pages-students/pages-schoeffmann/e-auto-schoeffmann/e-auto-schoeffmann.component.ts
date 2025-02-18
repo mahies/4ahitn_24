3ef4ecae1dc2fddab04fedda74c457c179421db4
theBroGrammer2624 <anonymistdumm@gmail.com>
2025-02-18 10:24:36 +0100

    AP-47

---
+++ src/app/pages/pages-students/pages-schoeffmann/e-auto-schoeffmann/e-auto-schoeffmann.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-e-auto-schoeffmann',
+  imports: [],
+  templateUrl: './e-auto-schoeffmann.component.html',
+  styleUrl: './e-auto-schoeffmann.component.scss'
+})
+export class EAutoSchoeffmannComponent {
+  kettcarimg: string = "assets-SCHOEFFMANN/Bilder/Hobbys/Kettcar.jpg";
+}
