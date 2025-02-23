c8be370209f726ff927dc5087209394854fcff22
karinaB777 <becker.karina@outlook.com>
2025-02-23 20:30:56 +0100

    userstorie 25

--- src/app/pages/pages-students/page-BECKER/page-about-meine-hobbys-becker/page-about-meine-hobbys-becker.component.ts
+++ src/app/pages/pages-students/page-BECKER/page-about-meine-hobbys-becker/page-about-meine-hobbys-becker.component.ts
@@ -8,5 +8,28 @@ import { CommonModule } from '@angular/common';
   styleUrl: './page-about-meine-hobbys-becker.component.scss'
 })
 export class PageAboutMeineHobbysBeckerComponent {
-
+  hobbies = [
+    {
+      name: 'Motorradfahren',
+      images: [
+        'assets-BECKER/gruppe.jpeg',
+        'assets-BECKER/gruppe2.jpeg',
+        'assets-BECKER/MesseM.jpeg',
+        'assets-BECKER/MotorradIch.jpeg',
+        'assets-BECKER/r125.jpeg',
+        'assets-BECKER/regenM.jpeg',
+        'assets-BECKER/teesdorf.jpeg'
+      ],
+      description: ''
+    },
+    {
+      name: 'Gym & Fitness',
+      images: [
+        'assets/hobbies/gym1.jpg',
+        'assets/hobbies/gym2.jpg',
+        'assets/hobbies/gym3.jpg'
+      ],
+      description: ''
+    }
+  ];
 }
