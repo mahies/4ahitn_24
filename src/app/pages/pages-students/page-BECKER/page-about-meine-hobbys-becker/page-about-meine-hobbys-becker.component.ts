3209a32da16791f84ddffbc81f737c4baa02c87a
karinaB777 <becker.karina@outlook.com>
2025-02-24 19:26:21 +0100

    userstorie 37

--- src/app/pages/pages-students/page-BECKER/page-about-meine-hobbys-becker/page-about-meine-hobbys-becker.component.ts
+++ src/app/pages/pages-students/page-BECKER/page-about-meine-hobbys-becker/page-about-meine-hobbys-becker.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {RouterLink, RouterLinkActive} from '@angular/router';
 
 @Component({
   selector: 'app-page-about-me-becker',
-  imports: [CommonModule],
+  imports: [CommonModule, RouterLink, RouterLinkActive],
   templateUrl: './page-about-meine-hobbys-becker.component.html',
   styleUrl: './page-about-meine-hobbys-becker.component.scss'
 })
--- src/app/pages/pages-students/page-BECKER/page-about-meine-reisen-becker/page-about-meine-reisen-becker.component.ts
+++ src/app/pages/pages-students/page-BECKER/page-about-meine-reisen-becker/page-about-meine-reisen-becker.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
-import {BECKER, Person} from '../../../../model/person';
 import { CommonModule } from '@angular/common';
+import {RouterLink, RouterLinkActive} from '@angular/router';
 
 @Component({
   selector: 'app-page-about-me-becker',
-  imports: [CommonModule],
+  imports: [CommonModule, RouterLink, RouterLinkActive],
   templateUrl: './page-about-meine-reisen-becker.component.html',
   styleUrl: './page-about-meine-reisen-becker.component.scss'
 })
