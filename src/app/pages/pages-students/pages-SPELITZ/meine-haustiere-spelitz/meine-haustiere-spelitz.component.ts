2f9d7001a8d40b453fbe9b65f17e085f990e2226
spelitz <fabianspelitz06@gmail.com>
2025-03-01 15:48:01 +0100

    FS-50

--- src/app/pages/pages-students/pages-SPELITZ/meine-haustiere-spelitz/meine-haustiere-spelitz.component.ts
+++ src/app/pages/pages-students/pages-SPELITZ/meine-haustiere-spelitz/meine-haustiere-spelitz.component.ts
@@ -1,8 +1,12 @@
 import { Component } from '@angular/core';
+import {RouterLink, RouterLinkActive} from "@angular/router";
 
 @Component({
   selector: 'app-meine-haustiere-spelitz',
-  imports: [],
+    imports: [
+        RouterLink,
+        RouterLinkActive
+    ],
   templateUrl: './meine-haustiere-spelitz.component.html',
   styleUrl: './meine-haustiere-spelitz.component.scss'
 })
