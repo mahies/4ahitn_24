8ec0c86c25a6006b73d369e8fdcd333e5ea35a0d
AJO7 <aljarrahalexander100@hotmail.com>
2025-03-04 11:21:06 +0100

    AlJarrah Fix

---
+++ src/app/pages/pages-students/pages-ALJARRAH/inspiration-aljarrah/inspiration-aljarrah.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-inspiration-aljarrah',
+  imports: [],
+  templateUrl: './inspiration-aljarrah.component.html',
+  styleUrl: './inspiration-aljarrah.component.scss'
+})
+export class InspirationAljarrahComponent {
+
+}
