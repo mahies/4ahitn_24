c26e89a2c0ae9becd903776b9054172e598a76e5
Julian Krapf <juliankrapf@icloud.com>
2025-03-05 00:59:45 +0100

    KRAPF: UB-58 bis UB-63

--- src/app/pages/pages-students/pages-KRAPF/header-krapf/header-krapf.component.ts
+++ src/app/pages/pages-students/pages-KRAPF/header-krapf/header-krapf.component.ts
@@ -1,8 +1,12 @@
 import { Component } from '@angular/core';
+import {RouterLink, RouterLinkActive} from '@angular/router';
 
 @Component({
   selector: 'app-header-krapf',
-  imports: [],
+  imports: [
+    RouterLink,
+    RouterLinkActive
+  ],
   templateUrl: './header-krapf.component.html',
   styleUrl: './header-krapf.component.scss'
 })
