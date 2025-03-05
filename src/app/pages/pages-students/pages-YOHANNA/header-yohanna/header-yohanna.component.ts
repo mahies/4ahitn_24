5ea52fb1afb52b695c041925da4a5ae568df6902
Adryano <adryanobutrus@gmail.com>
2025-03-05 01:43:32 +0100

    UB-57

--- src/app/pages/pages-students/pages-YOHANNA/header-yohanna/header-yohanna.component.ts
+++ src/app/pages/pages-students/pages-YOHANNA/header-yohanna/header-yohanna.component.ts
@@ -1,8 +1,12 @@
 import { Component } from '@angular/core';
+import {RouterLink, RouterLinkActive} from '@angular/router';
 
 @Component({
   selector: 'app-header-yohanna',
-  imports: [],
+  imports: [
+    RouterLink,
+    RouterLinkActive
+  ],
   templateUrl: './header-yohanna.component.html',
   standalone: true,
   styleUrl: './header-yohanna.component.scss'
